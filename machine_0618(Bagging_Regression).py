3d13021ebf92b39c87368a412ea93d7c736f4c6c
minione-kim <ksm5394@gmail.com>
2023-06-18 13:53:46 +0900

    [머신러닝 프로세스] 앙상블 배깅(Bagging) - 회귀(Regression) - from sklearn.ensemble import BaggingRegressor

---
+++ machine_0618(Bagging_Regression).py
@@ -0,0 +1,38 @@
+### 회귀(Regression) ###
+
+# 분석데이터 준비
+import warnings
+warnings.filterwarnings("ignore")
+
+# 분석데이터 준비
+import pandas as pd
+data = pd.read_csv("file/housing_price.csv", encoding='utf-8')
+X = data[data.columns[1:5]]
+y = data[['house_value']]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaler.fit(X_train)
+X_scaled_train = scaler.transform(X_train)
+X_scaled_test = scaler.transform(X_test)
+
+# 기본모델 적용
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import BaggingRegressor
+model = BaggingRegressor(base_estimator=KNeighborsClassifier(), n_estimators=10, random_state=0)
+model.fit(X_scaled_train, y_train)
+pred_train = model.predict(X_scaled_train)
+pred_test = model.predict(X_scaled_test)
+print("훈련데이터 정확도 : ", model.score(X_scaled_train, y_train))
+print("테스트데이터 정확도 : ", model.score(X_scaled_test, y_test))
+
+# RMSE (Root Mean Squared Error)
+from sklearn.metrics import mean_squared_error
+import numpy as np
+MSE_train = mean_squared_error(y_train, pred_train)
+MSE_test = mean_squared_error(y_test, pred_test)
+print("훈련 데이터 RMSE : ", np.sqrt(MSE_train))
+print("테스트 데이터 RMSE : ", np.sqrt(MSE_test))
