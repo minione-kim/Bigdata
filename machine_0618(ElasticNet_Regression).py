915b16be02d571403cae3fb3907c44b143586701
minione-kim <ksm5394@gmail.com>
2023-06-18 23:50:17 +0900

    [머신러닝 프로세스] 엘라스틱넷(Elasticnet) - 회귀(Regression) - from sklearn.linear_model import ElasticNet

---
+++ machine_0618(ElasticNet_Regression).py
@@ -0,0 +1,58 @@
+### 회귀(Regression) ###
+
+# 분석데이터 준비
+import warnings
+warnings.filterwarnings("ignore")
+
+# 분석데이터 준비
+import pandas as pd
+data = pd.read_csv("file/housing_price.csv", encoding='utf-8')
+X = data[data.columns[1:5]]
+y = data[['house_value']]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+scaler.fit(X_train)
+X_scaled_train = scaler.transform(X_train)
+X_scaled_test = scaler.transform(X_test)
+
+# 기본모델 적용
+from sklearn.linear_model import ElasticNet
+model = ElasticNet()
+model.fit(X_scaled_train, y_train)
+pred_train = model.predict(X_scaled_train)
+pred_test = model.predict(X_scaled_test)
+print("훈련데이터 정확도 : ", model.score(X_scaled_train, y_train))
+print("테스트데이터 정확도 : ", model.score(X_scaled_test, y_test))
+
+# RMSE (Root Mean Squared Error)
+import numpy as np
+from sklearn.metrics import mean_squared_error
+MSE_train = mean_squared_error(y_train, pred_train)
+MSE_test = mean_squared_error(y_test, pred_test)
+print("훈련데이터 RMSE : ", np.sqrt(MSE_train))
+print("테스트데이터 RMSE : ", np.sqrt(MSE_test))
+
+# Grid Search
+param_grid = {'alpha' : [0.0, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 0.1, 0.5, 1.0, 2.0, 3.0]}
+from sklearn.model_selection import GridSearchCV
+grid_search = GridSearchCV(ElasticNet(), param_grid, cv=5)
+grid_search.fit(X_scaled_train, y_train)
+
+print("Best Parameter : {}".format(grid_search.best_params_))
+print("Best Score : {:.4f}".format(grid_search.best_score_))
+print("TestSet Score : {:.4f}".format(grid_search.score(X_scaled_test, y_test)))
+
+# Random Search
+from scipy.stats import randint
+param_distribs = {'alpha': randint(low=0.00001, high=10)}
+from sklearn.model_selection import RandomizedSearchCV
+random_search = RandomizedSearchCV(ElasticNet(), param_distributions=param_distribs, n_iter=100, cv=5)
+random_search.fit(X_scaled_train, y_train)
+
+print("Best Parameter : {}".format(random_search.best_params_))
+print("Best Score : {:.4f}".format(random_search.best_score_))
+print("TestSet Score : {:.4f}".format(random_search.score(X_scaled_test, y_test)))
