4c7cb89679bd149d6c09da606adf9c18d5dbddfe
minione-kim <ksm5394@gmail.com>
2023-06-18 20:24:47 +0900

    [머신러닝 프로세스] 앙상블 부스팅(Boosting) - 회귀(Regression) - from sklearn.ensemble import AdaBoostClassifier - from sklearn.ensemble import GradientBoostingClassifier

--- machine_0618(Boosting_Regression).py
+++ machine_0618(Boosting_Regression).py
@@ -19,22 +19,22 @@ scaler.fit(X_train)
 X_scaled_train = scaler.transform(X_train)
 X_scaled_test = scaler.transform(X_test)
 
-# # 기본모델 적용
-# from sklearn.ensemble import AdaBoostRegressor
-# model = AdaBoostRegressor(random_state=0, n_estimators=100)
-# model.fit(X_scaled_train, y_train)
-# pred_train = model.predict(X_scaled_train)
-# pred_test = model.predict(X_scaled_test)
-# print("훈련데이터 정확도 : ", model.score(X_scaled_train, y_train))
-# print("테스트데이터 정확도 : ", model.score(X_scaled_test, y_test))
-#
-# # RMSE (Root Mean Squared Error)
-# import numpy as np
-# from sklearn.metrics import mean_squared_error
-# MSE_train = mean_squared_error(y_train, pred_train)
-# MSE_test = mean_squared_error(y_test, pred_test)
-# print("훈련 데이터 RMSE : ", np.sqrt(MSE_train))
-# print("테스트 데이터 RMSE : ", np.sqrt(MSE_test))
+# 기본모델 적용
+from sklearn.ensemble import AdaBoostRegressor
+model = AdaBoostRegressor(random_state=0, n_estimators=100)
+model.fit(X_scaled_train, y_train)
+pred_train = model.predict(X_scaled_train)
+pred_test = model.predict(X_scaled_test)
+print("훈련데이터 정확도 : ", model.score(X_scaled_train, y_train))
+print("테스트데이터 정확도 : ", model.score(X_scaled_test, y_test))
+
+# RMSE (Root Mean Squared Error)
+import numpy as np
+from sklearn.metrics import mean_squared_error
+MSE_train = mean_squared_error(y_train, pred_train)
+MSE_test = mean_squared_error(y_test, pred_test)
+print("훈련 데이터 RMSE : ", np.sqrt(MSE_train))
+print("테스트 데이터 RMSE : ", np.sqrt(MSE_test))
 
 # 기본모델 적용
 from sklearn.ensemble import GradientBoostingRegressor
